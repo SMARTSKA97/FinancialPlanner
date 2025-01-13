bc552c7a1ee7f5ec26b5f8284ce926d7b8b6e5c2
S KA <30918861+SMARTSKA97@users.noreply.github.com>
2025-01-13 23:15:50 +0530

    Added Saving Component

---
+++ FrontEnd/src/app/components/saving/saving.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saving',
+  imports: [],
+  templateUrl: './saving.component.html',
+  styleUrl: './saving.component.scss'
+})
+export class SavingComponent {
+
+}
